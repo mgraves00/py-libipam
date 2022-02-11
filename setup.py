be745b00d84bfc01eff600bc85e7c2c764de8a90
Michael Graves <30810535+mgraves00@users.noreply.github.com>
2022-02-10 22:16:32 -0600

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup( name="libipam",
-	version="1.0.0",
+	version="1.0.2",
 	author="Michael Graves",
 	author_email="mg@brainfat.net",
 	packages=find_packages("src"),
