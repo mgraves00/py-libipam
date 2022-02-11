a3a0c65eb4e84ddfda496b3eccb77223f3f9c2c6
Michael Graves <30810535+mgraves00@users.noreply.github.com>
2022-02-11 08:38:10 -0600

    fix copyright

--- src/libipam/__init__.py
+++ src/libipam/__init__.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2022 Michael Graves <mgraves@brainfat.net>
+# Copyright 2022 Michael Graves <mg@brainfat.net>
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
