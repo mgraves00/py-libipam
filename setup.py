293f4ca794a1e6840a5fa4a754eecd7c72dd7c62
user <noone@no.where>
2026-02-09 22:31:27 +0000

    update setup with new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup( name="libipam",
-	version="1.0.10",
+	version="1.0.11",
 	author="Michael Graves",
 	author_email="code@brainfat.net",
 	packages=find_packages("src"),
