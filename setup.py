9256ff43350107c17918c4b62631de2bfc06c885
user <noone@no.where>
2026-09-13 13:44:49 +0000

    touch version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup( name="libipam",
-	version="1.0.13",
+	version="1.0.14",
 	author="Michael Graves",
 	author_email="code@brainfat.net",
 	packages=find_packages("src"),
